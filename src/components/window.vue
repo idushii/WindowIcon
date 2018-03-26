af76ea87c8b05f944a04778aa98d3cdc4628c6f7
Nicolay <nic.nic343@ya.ru>
2018-03-26 16:03:03 +0700

    Реализациямаршрутизаторов. Вынес отдельно окно, вычисление значений для окна

---
+++ src/components/window.vue
@@ -0,0 +1,60 @@
+<template>
+  <svg :style="{ width: window.w, height: window.h }">
+    <rect class="window-frame" :width=window.w :height=window.h />
+    <rect class="window" :width=pointsGlass.glass.width :height=pointsGlass.glass.height :x=pointsGlass.glass.x :y=pointsGlass.glass.y />
+    <polygon :style="{'stroke-width': propsSVG.line.width*1.5}" class="window-glass-border" :points=pointsGlass.border />
+    <polyline :points="pointsFrame" class="window-open-line" :style="{'stroke-width': propsSVG.line.width}" />
+    <polygon v-if="window.type == 'active'" :points="pointsOpen" class="window-open-line" :style="{'stroke-width': propsSVG.line.width}" />
+    <polygon v-if="window.type == 'active' && window.leaf" :points="pointsLeaf" class="window-open-line" :style="{'stroke-width': propsSVG.line.width}" />
+    <image id="pen" v-if="window.type == 'active'" xlink:href="./../assets/pen.png" :x=positionPen.x :y=positionPen.y :width="propsSVG.frame.width*0.75+'px'"/>
+  </svg>
+</template>
+
+<script>
+  export default {
+    name: 'window',
+    props: {
+      window: {
+        type: Object,
+        default: () => ({ w: 100, h: 100, type: 'static', leaf: false })
+      },
+      propsSVG: {},
+      pointsGlass: {},
+      pointsOpen: {},
+      pointsLeaf: {},
+      pointsFrame: {},
+      positionPen: {},
+    },
+    data() {
+      return {}
+    },
+    computed: {},
+    mounted() {},
+    methods: {}
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang=scss>
+$window-color: rgb(189, 247, 255);
+$frame-color: rgb(224, 234, 238);
+$window-line-color: rgb(94, 133, 138);
+
+.window { fill: $window-color; }
+.window-open-line { 
+  fill: transparent; 
+  stroke: $window-line-color; 
+  stroke-width: 1;
+}
+
+.window-glass-border {
+  fill: transparent; 
+  stroke: $window-line-color; 
+  stroke-width: 1;
+}
+
+.window-frame {
+  fill: $frame-color;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
+import editOneWindow from './Pages/editOneWindow.vue'
+import window from './components/window.vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 import axios from 'axios'
 
-require('./myComponents/index.js')
+require('vue-bootstrap-elements')
+Vue.component('window', window)
+
+let routes = [
+  {
+    path: '/',
+    name: 'home',
+    redirect: '/one/phone'
+  },
+  {
+    path: '/one',
+    name: 'editOneWindow',
+    component: editOneWindow
+  },
+  {
+    path: '/one/phone',
+    name: 'editPhoneOneWindow',
+    component: editOneWindow
+  }
+]
+
+const router = new VueRouter({
+  routes // сокращение от `routes: routes`
+})
 
 new Vue({
   el: '#app',
@@ -11,5 +39,6 @@ new Vue({
   data: {
     url_files: 'http://localhost/Stat/Data/',
     url_img: 'http://localhost/Stat/src/assets/Data/img'
-  }
+  },
+  router
 })
